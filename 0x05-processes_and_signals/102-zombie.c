58704d2a157ba9b5ec8d49dea112dd448d24046e
cherono-code <cheronodaisy38@gmail.com>
2023-07-31 13:53:35 -0700

    task

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - creates an infinite loop to make the program hang
+ * Return: always 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - creates 5 zombie processes
+ * Return: always 0
+ */
+int main(void)
+{
+	int e;
+	pid_t zombie;
+
+	for (e = 0; e < 5; e++)
+	{
+		zombie = fork();
+		if (!zombie)
+			return (0);
+		printf("Zombie process created, PID: %d\n", zombie);
+	}
+
+	infinite_while();
+	return (0);
+}
